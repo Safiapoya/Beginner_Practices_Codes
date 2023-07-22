e057e8a96db0603aa103db9d06423b02db5a4c4b
Safiapoya <139712358+Safiapoya@users.noreply.github.com>
2023-07-22 17:16:20 +0200

    Add files via upload

---
+++ Calculate_Area_of_Rectangle.py
@@ -0,0 +1,6 @@
+#Calculate the area of a rectangle:
+length = float(input("What is the length of rectangle?\n"))
+width = float(input("What is the width of rectangle?\n"))
+#area of rectangle = length × width
+area = (width*length)
+print("The area of rectangle is:{}".format(area))
