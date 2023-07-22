ce9cdd757fd5bef638ec03b3d875c68a1d150776
Safiapoya <139712358+Safiapoya@users.noreply.github.com>
2023-07-22 20:13:49 +0200

    Add files via upload

---
+++ Compute_MPG_Car.py
@@ -0,0 +1,4 @@
+miles =int(input("Total miles driven: "))
+gallons =int(input("Total gallons of fuel used: "))
+MPG = miles/gallons
+print("The MPG of {} miles and {} gallons will be {} .".format(miles, gallons, MPG))
